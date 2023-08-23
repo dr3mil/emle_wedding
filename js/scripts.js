0210593655e39575352a19f5f4b1a0d9cf2b4648
dr3mil <emil.sauter@gmail.com>
2023-08-23 21:48:27 +0200

    Edited FAQs; Adapted POST

--- js/scripts.js
+++ js/scripts.js
@@ -217,7 +217,7 @@ $(document).ready(function () {
         //if (MD5($('#invite_code').val()) !== '15f59a195dda7dfb4e803b1ab432477e') {
         //    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         //} else {
-        $.post('https://script.google.com/macros/s/AKfycbxx4NWSWwy4hIHaIhkRJXSkwG2GQPxuG7dz_o0MLuwQDYw9NJ3XBALTO0O8msQ3VanVIQ/exec', data)
+        $.post('https://script.google.com/macros/s/AKfycbzoO6J8LdOBr0foQ8rMPrDuJYZ0C0PPJFOUGvmUfNkLmXfhrPJ98w9al2poZo0H7cjDxw/exec', data)
             .done(function (data) {
                 console.log(data);
                 if (data.result === "error") {
